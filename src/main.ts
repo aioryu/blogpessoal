7f78a39210e9040b5e3a917b1321a6ff6b0f1e0d
Vitor Nazareth <61162481+aioryu@users.noreply.github.com>
2025-09-05 10:09:26 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,11 @@ process.env.TZ = '-03:00';
 
 app.useGlobalPipes(new ValidationPipe());
 
+    app.enableCors();
+  
   await app.listen(process.env.PORT ?? 4000);
 
-  app.enableCors();
+
 
   }
 bootstrap();
